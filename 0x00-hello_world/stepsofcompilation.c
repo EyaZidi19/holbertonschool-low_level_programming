86dcfdb587362bfa31aa01a0ff0cad8bc3d27d55
EyaZidi19 <3805@holbertonschool.com>
2021-09-30 03:19:12 -0700

    stepsofcompilation.c

---
+++ 0x00-hello_world/stepsofcompilation.c
@@ -0,0 +1,2 @@
+https://www.linkedin.com/posts/eya-zidi-b5b985221_how-the-compilation-process-works-for-c-programs-activity-6849284002294059008-l8JU
+https://medium.com/@3805/how-the-compilation-process-works-for-c-programs-ad6d62aa831
