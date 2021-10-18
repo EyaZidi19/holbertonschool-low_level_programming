a5c0881143293218e02776644ec44684f25774ed
EyaZidi19 <3805@holbertonschool.com>
2021-10-18 10:22:02 -0700

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,19 +15,4 @@ char *_strchr(char *s, char c)
 	if (*(s + i) == c)
 		return (s + i);
 	return ('\0');
-}trchr - locates a character in a string,
-  * @s: string.
-   * @c: character.
-   * Return: the pointer to the first occurrence of the character c.
-    */
-   char *_strchr(char *s, char c)
-   {
-     unsigned int i = 0;
-
-     for (; *(s + i) != '\0'; i++)
-       if (*(s + i) == c)
-	 return (s + i);
-     if (*(s + i) == c)
-       return (s + i);
-     return ('\0');
-   }~
+}
